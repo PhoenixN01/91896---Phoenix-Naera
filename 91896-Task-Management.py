cb5d5fb509df4609e06331ccba8485bff405e32f
Phoenix N <pn22178@my.westlake.school.nz>
2025-06-16 12:01:31 +1200

    Started setting up basic cases for tasks and team members dictionaries

---
+++ 91896-Task-Management.py
@@ -0,0 +1,47 @@
+import easygui
+
+task_dictionary = {
+    "T1": {
+        "Title": "Design Homepage",
+        "Description": "Create a mockup of the homepage",
+        "Assignee": "JSM",
+        "Priority": 3,
+        "Status": "In Progress"
+        },
+    "T2": {
+        "Title": "Implement Login Page",
+        "Description": "Create the Login Page for the website",
+        "Assignee": "JSM",
+        "Priority": 3,
+        "Status": "Blocked"
+        },
+    "T3": {
+        "Title": "Fix Navigation Menu",
+        "Description": "Fix the navigation menu to be more user-friendly",
+        "Assignee": "",
+        "Priority": 1,
+        "Status": "Not Started"
+        },
+    "T4": {
+        "Title": "Add Payment Process",
+        "Description": "Implement payment processing for the website",
+        "Assignee": "JLO",
+        "Priority": 2,
+        "Status": "In Progress"
+        },
+    "T5": {
+        "Title": "Create an About Us page",
+        "Description": "Create a page with information about the company",
+        "Assignee": "BDI",
+        "Priority": 1,
+        "Status": "Blocked"
+        }
+}
+
+team_members_dictionary = {
+    "JSM": {
+        "Name": "John Smith",
+        "Email": "John@techvision.com"
+        "Tasks Assigned"
+    }
+}
